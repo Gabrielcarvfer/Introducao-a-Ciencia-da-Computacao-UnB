3538538504f83163b8053a36ad05754615c671e5
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2018-06-19 19:13:35 -0300

    Manipulação arquivos CSV

---
+++ trabalho/manipulacao_de_arquivos/arquivos.py
@@ -0,0 +1,15 @@
+
+
+import csv
+
+arquivo = csv.DictReader(open("teste.csv","r",encoding="utf8"))
+
+print("Cabeçalhos/chaves dos dicionários")
+print(arquivo.fieldnames)
+print()
+
+for linha in arquivo:
+    print(linha)
+    for campo in arquivo.fieldnames:
+        print(campo, ':', linha[campo])
+    print()
